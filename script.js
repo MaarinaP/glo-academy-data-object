5cd07a6cd0c900376c028557cadfb87ef8f40445
Marina Pavlova <marinap.learning@gmail.com>
2022-02-27 15:31:04 +0000

    Add Intl.DateTimeFormat objects and show results to the page

--- script.js
+++ script.js
@@ -3,16 +3,59 @@
 //lesson 9
 console.log("\n lesson 9 ------------\n");
 
+
+
+
+
+
+
+// console.log(today);
+
+// console.log(new Date().toLocaleString('ru', {  weekday: 'long' }));
+
+
+// let dayOfTheWeek = (day, month, year) => {
+//     return new Date(year, month - 1, day).toLocaleString("ru", {
+//       weekday: "long",
+//     });
+//   };
+  
+//   console.log(dayOfTheWeek(3, 11, 2020));
+
+//   new Intl.DateTimeFormat(["ru"[, options]])
+// const date = today.getDate();
+// const day = today.toLocaleString("ru", {weekday: "long"});
+
+//   console.log("Сегодня " + day + ", " + date);
+
 let today = new Date();
-let today2 = new Date();
 
 let place1 = document.getElementById("item1");
 let place2 = document.getElementById("item2");
 
-today.toLocaleDateString('ru-RU');
-today2.toLocaleDateString('ru-RU', {year: 'numeric', month: '2-digit', day:'2-digit'});
+let options1 = {
+  weekday: 'long', 
+  year: 'numeric', 
+  month: 'long', 
+  day: 'numeric',
+  hour: 'numeric', 
+  minute: 'numeric', 
+  second: 'numeric'
+};
 
-place1.textContent = today;
-place2.textContent = today2;
+let options2 = {
+  year: 'numeric', 
+  month: '2-digit', 
+  day: 'numeric',
+  hour: '2-digit', 
+  minute: '2-digit', 
+  second: 'numeric'
+};
 
-console.log(today);
+let date = new Intl.DateTimeFormat("ru-ru", options1).format(today);
+let date2 = new Intl.DateTimeFormat("ru-ru", options2).format(today);
+
+place1.textContent = date;
+place2.textContent = date2;
+
+
